d4ac7bf4a56bbb839983e2177603be4ef6f2b9bd
stayadi07 <stayadi007@gmail.com>
2022-03-06 14:22:00 +0530

    create_object submission

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,10 @@
 function CreateObject(arr) {
     // Write your code here
+    let obj = {}
+    for(let i =0; i<arr.length;i=i+2){
+        obj[arr[i]]=arr[i+1]
+    }
+    return obj
 }
 
 module.exports = CreateObject;
