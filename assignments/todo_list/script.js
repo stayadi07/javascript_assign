2bbb4cd5acfa52e8302b75e12565c7d34d7b6a45
stayadi07 <stayadi007@gmail.com>
2022-03-13 00:16:08 +0530

    Assignment8 submission

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,25 +1,36 @@
-let Todo = document.querySelector('Input');
-let add_button = document.querySelector('button');
-let todolist = document.querySelector('div.todos');
+let divcontainer=document.getElementById('container');
+let select=document.getElementById('todo');
+let completedtodo=[];
+let pendingtodo=[];
 
-let count =0;
-
-function Add_Todo(){
-
-    let input_todo = Todo.value;
-    if(input_todo==""){
-        return
+fetch('https://jsonplaceholder.typicode.com/todos')  
+.then(response => response.json())
+.then(data => {
+    for(let i=0;i<data.length;i++){
+        if(data[i].completed==false){
+            pendingtodo.push(data[i].title);
+        }else{
+            completedtodo.push(data[i].title);
+        }
     }
-    let item = document.createElement('p');
-    item.innerHTML= input_todo;
-    item.setAttribute("key",count);
-    todolist.append(item);
-    count+=1;
-    item.addEventListener("click",function(){
-        todolist.removeChild(item);
-    })
-    Todo.value = ""
-}
-add_button.addEventListener("click",Add_Todo)
+})
 
 
+select.addEventListener('change',function(){
+    let value1=select.value;
+    if(value1==="Completed"){
+        divcontainer.innerHTML="SHOW COMPLETED TODO";
+        for(let i=0;i<completedtodo.length;i++){
+            let tag1=document.createElement('li');
+            tag1.innerHTML=completedtodo[i];
+            divcontainer.appendChild(tag1);
+        }
+    }else if(value1==="pending"){
+        divcontainer.innerHTML="SHOW PENDING TODO";
+        for(let i=0;i<pendingtodo.length;i++){
+            let tag1=document.createElement('li');
+            tag1.innerHTML=pendingtodo[i];
+            divcontainer.appendChild(tag1);
+        }
+    }
+})
